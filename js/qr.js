58e4eaacb5e26c51a19cd15517a647ab2396d6a8
Brandon <vaulter82@gmail.com>
2018-03-16 17:20:08 -0500

    qr.js: hide initialize button on click, add message div, show scanned data
    delete Controller folder (not needed)

--- js/qr.js
+++ js/qr.js
@@ -7,16 +7,19 @@
       requestAnimationFrame(tick);
     });
   }
-  
-  $('body').append('<button id="initialize_scan_button" style="position:fixed;bottom:15px;right:15px;height:30px;width:30px;">Scan</button>');
-  $('body').append('<canvas id="pole_manager_code_reader_canvas" style="display:none;position:fixed;height:100%;width:100%"></canvas>');
-  
+
+  $('body').append('<button id="initialize_scan_button" style="position:fixed;bottom:15px;right:15px;height:30px;width:50px;">Scan</button>');
+  $('body').append('<canvas id="pole_manager_code_reader_canvas" style="display:none;position:fixed;top:0px;left:0px;height:90%;width:100%"></canvas>');
+  $('body').append('<div id="pole_manager_code_reader_message" style="display:none;position:fixed;bottom:0px;left:0px;height:10%;width:100%"></div>')
+
   var video = document.createElement("video");
   var canvasElement = document.getElementById("pole_manager_code_reader_canvas");
   var canvas = canvasElement.getContext("2d");
-  
-  document.getElementById("initialize_scan_button").onclick = init;
-  
+  var message = document.getElementById("pole_manager_code_reader_message");
+  var initializeButton = document.getElementById("initialize_scan_button");
+
+  initializeButton.onclick = init;
+
   function drawLine(begin, end, color) {
     canvas.beginPath();
     canvas.moveTo(begin.x, begin.y);
@@ -25,25 +28,31 @@
     canvas.strokeStyle = color;
     canvas.stroke();
   }
-  
+
   function tick() {
     if( video.readyState === video.HAVE_ENOUGH_DATA ) {
+      initializeButton.style.display = "none";
+      message.style.display = "initial";
       canvasElement.style.display = "initial";
       canvasElement.height = video.videoHeight;
       canvasElement.width = video.videoWidth;
       canvas.drawImage(video, 0, 0, canvasElement.width, canvasElement.height);
-      
+
       var imageData = canvas.getImageData(0, 0, canvasElement.width, canvasElement.height);
       var code = jsQR(imageData.data, imageData.width, imageData.height);
-      
+
       if( code ) {
         drawLine(code.location.topLeftCorner, code.location.topRightCorner, "#FF3B58");
         drawLine(code.location.topRightCorner, code.location.bottomRightCorner, "#FF3B58");
         drawLine(code.location.bottomLeftCorner, code.location.bottomRightCorner, "#FF3B58");
         drawLine(code.location.topLeftCorner, code.location.bottomleftCorner, "#FF3B58");
+
+        message.innerText = code.data;
+      } else {
+        
       }
     }
-    
+
     requestAnimationFrame(tick);
   }
 }(jQuery, Drupal));
