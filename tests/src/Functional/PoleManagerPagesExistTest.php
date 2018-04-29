8f16f2630fbd8b8c1d0e983a2fb31a6dc42b42ee
Brandon <vaulter82@gmail.com>
2018-04-28 19:36:26 -0500

    add group annotation to test class

--- tests/src/Functional/PoleManagerPagesExistTest.php
+++ tests/src/Functional/PoleManagerPagesExistTest.php
@@ -4,9 +4,12 @@ namespace Drupal\Tests\pole_manager\Functional;
 
 use Drupal\Tests\BrowserTestBase;
 
+/**
+ * @group pole_manager
+ */
 class PoleManagerPagesExistTest extends BrowserTestBase {
   protected static $modules = ['pole_manager'];
-  
+
   public function testPoleCollectionPage() {
     $this->drupalGet('poles/collection');
     $this->assertSession()->statusCodeEquals(200);
